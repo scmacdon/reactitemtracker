892f76672b334d0bf2b23d832307d4fa72a8dce7
scmacdon <scmacdon@amazon.com>
2024-05-01 16:44:03 -0400

    modified follow

--- src/Follow.js
+++ src/Follow.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
-import './styles.css';
 
 const Follow = () => {
   const [itemId, setItemId] = useState('');
   const [itemData, setItemData] = useState({});
   const [formErrors, setFormErrors] = useState({ itemId: '' });
-  const [isUpdating, setIsUpdating] = useState(false);
   const [isFetching, setIsFetching] = useState(false);
-  const [isCreatingFollow, setIsCreatingFollow] = useState(false); // New state for tracking follow creation
 
   const handleInputChange = (e) => {
     const { value } = e.target;
@@ -20,13 +17,18 @@ const Follow = () => {
 
   const handleGetItem = async (e) => {
     e.preventDefault();
+
     const itemIdFromInput = itemId.trim();
+
     try {
       setIsFetching(true);
+
       const response = await fetch(`http://localhost:8080/api/items/${itemIdFromInput}`);
+
       if (!response.ok) {
         throw new Error('Failed to retrieve item data');
       }
+
       const responseDataString = await response.json();
       setItemData(responseDataString);
     } catch (error) {
@@ -36,11 +38,10 @@ const Follow = () => {
     }
   };
 
-  const updateFormData = async (e) => {
+  const handleCreateFollow = async (e) => {
     e.preventDefault();
     try {
-      setIsUpdating(true);
-      const { SOS, ...dataToSend } = itemData;
+      const { Engineer, Language, ...dataToSend } = itemData; // Only sending Engineer and Language
       const requestOptions = {
         method: 'POST',
         headers: {
@@ -48,10 +49,9 @@ const Follow = () => {
         },
         body: JSON.stringify(dataToSend),
       };
-      const response = await fetch(
-        'https://tvo3zd1on3.execute-api.us-east-1.amazonaws.com/updateitem',
-        requestOptions
-      );
+
+      const response = await fetch('https://4zzkjzebl8.execute-api.us-east-1.amazonaws.com/Follow', requestOptions);
+
       if (response.ok) {
         const successText = await response.text();
         alert(successText);
@@ -60,37 +60,6 @@ const Follow = () => {
       }
     } catch (error) {
       console.error('Error updating item:', error.message);
-    } finally {
-      setIsUpdating(false);
-    }
-  };
-
-  const handleCreateFollow = async (e) => {
-    e.preventDefault();
-    try {
-      setIsCreatingFollow(true); // Start the loading indicator
-      const { sos, status, ...dataToSend } = itemData;
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(dataToSend),
-      };
-      const response = await fetch(
-        'https://4zzkjzebl8.execute-api.us-east-1.amazonaws.com/Follow',
-        requestOptions
-      );
-      if (response.ok) {
-        const successText = await response.text();
-        alert(successText);
-      } else {
-        throw new Error('Failed to create follow');
-      }
-    } catch (error) {
-      console.error('Error creating follow:', error.message);
-    } finally {
-      setIsCreatingFollow(false); // Stop the loading indicator
     }
   };
 
@@ -105,6 +74,7 @@ const Follow = () => {
           <input type="text" id="itemId" name="itemId" value={itemId} onChange={handleInputChange} />
           <span className="error-message">{formErrors.itemId}</span>
         </div>
+
         <div>
           <button type="button" onClick={handleGetItem} disabled={isFetching}>
             {isFetching ? 'Fetching Data...' : 'Get Follow Data'}
@@ -113,28 +83,22 @@ const Follow = () => {
       </form>
 
       {Object.keys(itemData).length > 0 && (
-        <form onSubmit={updateFormData}>
-          {Object.keys(itemData)
-            .filter((key) => key !== 'Sos' && key !== 'Service2') // Filter out SOS and Service2 fields
-            .filter((key) => key !== 'status') // Filter out status field
-            .sort()
-            .map((key) => (
-              <div key={key}>
-                <label htmlFor={key}>{key.charAt(0).toUpperCase() + key.slice(1)}:</label>
-                <input
-                  type="text"
-                  id={key}
-                  name={key}
-                  readOnly={key === 'id'} // Make only the itemId field read-only
-                  value={itemData[key]}
-                  onChange={(e) => setItemData({ ...itemData, [key]: e.target.value })}
-                />
-              </div>
-            ))}
+        <form onSubmit={handleCreateFollow}>
+          {Object.keys(itemData).map((key) => (
+            <div key={key}>
+              <label htmlFor={key}>{key.charAt(0).toUpperCase() + key.slice(1)}:</label>
+              <input
+                type="text"
+                id={key}
+                name={key}
+                readOnly={key !== 'engineer' && key !== 'language'} // Make all fields readonly except Engineer and Language
+                value={itemData[key]}
+                onChange={(e) => setItemData({ ...itemData, [key]: e.target.value })}
+              />
+            </div>
+          ))}
           <div>
-            <button type="button" onClick={handleCreateFollow} disabled={isCreatingFollow}>
-              {isCreatingFollow ? 'Creating Follow...' : 'Create Follow'}
-            </button>
+            <button type="submit">Create Follow</button>
           </div>
         </form>
       )}
@@ -143,3 +107,4 @@ const Follow = () => {
 };
 
 export default Follow;
+
