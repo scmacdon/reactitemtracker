0ab1032941ea45efd287c1b7081ae1a29f1a4530
scmacdon <scmacdon@amazon.com>
2024-05-01 16:06:18 -0400

    modified rubric

--- src/Rubric.js
+++ src/Rubric.js
@@ -1,16 +1,13 @@
 import React, { useState } from 'react';
-import Modal from 'react-modal';
-
-Modal.setAppElement('#root');
 
 const PageResearch = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [formData, setFormData] = useState({});
   const [emptyFields, setEmptyFields] = useState([]);
   const [serverResponse, setServerResponse] = useState({
     statusCode: null,
     responseBody: null,
   });
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const fields = [
     'Item Id',
@@ -31,22 +28,6 @@ const PageResearch = () => {
   const handleFieldChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [fieldMappings[name]]: value });
-  };
-
-  const openModal = () => {
-    // Check for empty fields before opening the modal
-    const emptyFieldsList = fields.filter((field) => !formData[fieldMappings[field]]);
-    setEmptyFields(emptyFieldsList);
-
-    if (emptyFieldsList.length === 0 && formData.itemId) {
-      setIsModalOpen(true);
-    }
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setFormData({});
-    setEmptyFields([]);
   };
 
   const renderFormFields = () => {
@@ -77,21 +58,12 @@ const PageResearch = () => {
 
   const handleSave = async () => {
     try {
-      // Check if Item Id is empty
+      setIsSubmitting(true);
       if (!formData.itemId) {
         alert("Please fill out the 'Item Id' field.");
         return;
       }
   
-      // Display a confirmation dialog
-      const shouldSubmit = window.confirm('Are you sure you want to submit the Rubric data?');
-  
-      if (!shouldSubmit) {
-        // If the user clicks "Cancel," do not submit data
-        return;
-      }
-  
-      // Create a copy of formData with all fields filled with 'No' as default
       const formDataCopy = { ...formData };
       fields.forEach(field => {
         if (!(fieldMappings[field] in formDataCopy)) {
@@ -99,8 +71,7 @@ const PageResearch = () => {
         }
       });
   
-     // const response = await fetch('http://localhost:8080/api/items/rubric',{
-      const response = await fetch('https://tat7tu91yf.execute-api.us-east-1.amazonaws.com/Rubric',{
+      const response = await fetch('https://tat7tu91yf.execute-api.us-east-1.amazonaws.com/Rubric', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -108,18 +79,17 @@ const PageResearch = () => {
         body: JSON.stringify(formDataCopy),
       });
   
-      setServerResponse({
-        statusCode: response.status,
-        responseBody: await response.text(),
-      });
+      const responseBody = await response.text();
+      alert(responseBody);
   
-      setIsModalOpen(true);
+      setIsSubmitting(false);
     } catch (error) {
       console.error('Error submitting form data:', error);
-      // Handle any unexpected errors
     }
   };
   
+
+
 
   return (
     <div>
@@ -127,56 +97,31 @@ const PageResearch = () => {
       <p>
         An SDK getting started scenario uses a single AWS service (or part of a service) to show new AWS developers how to get up and running using the SDK and the service.
 
-        The rubric can be applied to any candidate AWS Service that meets the following go/no-go criteria:
+        The rubric can be applied to any Getting Started item that is in <b>Draft</b> state. If the item is not
+        in this state, you cannot run a rubric on it. 
+        The Getting Started item also meets the following go/no-go criteria:
       </p>
       <ul>
-        <li>The service does not have an existing getting-started-style scenario in the Code Library.</li>
-        <li>The service is included in the list of Onboard Candidates.</li>
-        <li>The service is in the top 50 services based on data metrics or in the original MVP candidate list. (OR is a new service?)</li>
+        <li>The service does not have an existing getting-started-style scenario in the Code Library. If it does, move on to the next service.</li>
+        <li>The service is included in the list of Code Examples Roadmap (see link in process page).</li>
+        <li>The service is in the top 50 services based on data metrics or in the original candidate list.</li>
       </ul>
 
       <div>{renderFormFields()}</div>
-      <button onClick={handleSave}>Submit</button>
+      <button onClick={handleSave} disabled={isSubmitting}>
+        {isSubmitting ? 'Submitting...' : 'Calculate Rubric'}
+      </button>
 
       {emptyFields.length > 0 && (
         <p style={{ color: 'red', marginTop: '10px' }}>
           Please fill out all fields: {emptyFields.concat(!formData.itemId ? 'Item Id' : []).join(', ')}
         </p>
       )}
-
-      {isModalOpen && (
-        <Modal
-          isOpen={isModalOpen}
-          onRequestClose={closeModal}
-          contentLabel={`Submit Confirmation`}
-          style={{
-            overlay: {
-              backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            },
-            content: {
-              width: '60%',
-              margin: 'auto',
-              borderRadius: '10px',
-              boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
-            },
-          }}
-        >
-          <button onClick={closeModal} style={{ float: 'right', margin: '10px', padding: '5px' }}>
-            Close
-          </button>
-          <div style={{ padding: '20px' }}>
-            <h2>Server Response</h2>
-            <div style={{ marginBottom: '10px' }}>
-              <strong>Status Code:</strong> {serverResponse.statusCode}
-            </div>
-            <div>
-              <strong>Response Body:</strong> {serverResponse.responseBody}
-            </div>
-          </div>
-        </Modal>
-      )}
     </div>
   );
 };
 
 export default PageResearch;
+
+
+
